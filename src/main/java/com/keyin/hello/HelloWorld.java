f4a3dd3faf728e73d434bb15837a6f7e04879023
jamie.cornick <jamie.cornick@keyin.com>
2023-10-13 10:14:12 -0230

    Initial commit

---
+++ src/main/java/com/keyin/hello/HelloWorld.java
@@ -0,0 +1,27 @@
+package com.keyin.hello;
+
+public class HelloWorld {
+    private long id;
+    private String content;
+
+    public HelloWorld(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
---
+++ src/main/java/com/keyin/hello/HelloWorldController.java
@@ -0,0 +1,20 @@
+package com.keyin.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class HelloWorldController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/hello_world")
+    public HelloWorld greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new HelloWorld(counter.incrementAndGet(), String.format(template, name));
+    }
+
+}
